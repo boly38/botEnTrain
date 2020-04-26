0f3292b29bafc90a33504825909bc6f019a3a504
Brice Vandeputte <brice.vandeputte@orange.com>
2020-04-26 23:18:46 +0200

    Fix #12 bot hook

--- NewsBot.js
+++ NewsBot.js
@@ -3,7 +3,7 @@ const dateFormat = require('dateformat');
 
 class NewsBot {
   constructor(maxBufferEntries) {
-    this.max = maxBufferEntries && maxBufferEntries > 0 ? maxBufferEntries : 30;
+    this.max = maxBufferEntries && maxBufferEntries > 0 ? maxBufferEntries : 10;
     this.lastNews = [];
   }
 
@@ -11,7 +11,7 @@ class NewsBot {
     var day = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
     this.lastNews.splice(0, 0, day + " | " + news);
     if (this.lastNews.length > this.max) {
-        thisl.lastNews.splice(0, 1);
+        this.lastNews.splice(this.lastNews.length-1, 1);
     }
   }
 
