35a12160f73a18dde69f652d9e8ce49c331b8503
Brice Vandeputte <brice.vandeputte@orange.com>
2020-04-27 13:10:48 +0200

    Fix #14 no ip

--- BotEngine.js
+++ BotEngine.js
@@ -1,10 +1,13 @@
 /*jshint esversion: 6 */
+var log4js = require('log4js');
 
 // const ENGINE_MIN_INTERVAL_MS=6000;//6 seconds // DEV MODE
 const ENGINE_MIN_INTERVAL_MS=60*60000;// min 1 each hour*
 
 class BotEngine {
   constructor(newsBotDep, engineMinIntervalMs) {
+    this.logger = log4js.getLogger();
+    this.logger.setLevel('INFO');
     this.newsBot = newsBotDep;
     this.intervalMs = engineMinIntervalMs && engineMinIntervalMs >= ENGINE_MIN_INTERVAL_MS ?
         engineMinIntervalMs : ENGINE_MIN_INTERVAL_MS;
@@ -20,11 +23,12 @@ class BotEngine {
     let nowMs = (new Date()).getTime();
     let allowedTs = engine.lastProcess + engine.intervalMs;
     if (engine.lastProcess && allowedTs > nowMs) {
-        console.info(remoteAdd + " | need to wait ", allowedTs - nowMs, "ms" );
+        engine.logger.info(remoteAdd + " | need to wait ", allowedTs - nowMs, "ms" );
         return;
     }
     engine.lastProcess = nowMs;
-    engine.newsBot.add(remoteAdd + " | process right now");
+    engine.logger.info(remoteAdd + " | process right now");
+    engine.newsBot.add("process right now");
   }
 }
 
