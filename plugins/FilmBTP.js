bd0f348eb94c8ecba151e35f3fa66a0544986408
Brice Vandeputte <brice.vandeputte@orange.com>
2020-04-27 21:55:02 +0200

    Fix #19 filter own tweets, upd style
    - houndci

--- plugins/FilmBTP.js
+++ plugins/FilmBTP.js
@@ -4,10 +4,10 @@ var log4js = require('log4js');
 class FilmBTP {
   constructor(twitterClient) {
     this.logger = log4js.getLogger();
-    this.logger.setLevel('INFO'); // DEBUG will show search results
+    this.logger.setLevel('DEBUG'); // DEBUG will show search results
     this.twitterClient = twitterClient;
     this.behaviors = [
-        { "search":"est très fin",
+        { "search":"ça se mange sans faim.", // "est très fin",
           "reply":"C'est fin, c'est très fin, ça se mange sans faim.",
           "link":"https://www.youtube.com/watch?v=xa2A07mhG9g"
         }
@@ -30,11 +30,14 @@ class FilmBTP {
             cb("unable to search tweet");
             return;
         }
+        /*
         let debugLog = "";
         tweets.forEach((t) => {
             debugLog += "\n\t" + this.twitterClient.tweetLinkOf(t) + "\n\t\t" + this.twitterClient.tweetInfoOf(t);
+            // debugLog += JSON.stringify(t) ;
         });
         this.logDebug("search results " + debugLog);
+        */
         let filteredTweets = tweets.filter((t) => {
             return t.text && t.text.includes(behavior.search);
         });
@@ -52,16 +55,47 @@ class FilmBTP {
                 cb("unable to reply tweet");
                 return;
             }
-            let successState = "Reply sent:" +
-                this.twitterClient.tweetLinkOf(replyTweet) +
-                this.twitterClient.tweetInfoOf(replyTweet);
-            cb(false, successState);
+            cb(false, {
+                "html": "<b>Tweet</b>: " +
+                    this.twitterClient.tweetHtmlOf(tweetCandidate) +
+                    "<br><b>Reply sent</b>:" +
+                    this.twitterClient.tweetHtmlOf(replyTweet),
+                "text": "\nTweet:\n\t" +
+                    this.twitterClient.tweetLinkOf(tweetCandidate) + "\n\t" +
+                    this.twitterClient.tweetInfoOf(tweetCandidate) + "\n" +
+                    "Reply sent:\n\t" +
+                    this.twitterClient.tweetLinkOf(replyTweet) + "\n\t" +
+                    this.twitterClient.tweetInfoOf(replyTweet) + "\n"
+            });
         });
     });
   }
 
   searchTweets(behavior, cb) {
-    this.twitterClient.search(behavior.search, 20, cb);
+    let plugin = this;
+    let noRetweet = " -filter:retweets";
+    let fromMe = " from:botEnTrain1";
+    let notMe = " -from:botEnTrain1";
+    let searchQueryFromMe = "\"" + behavior.search + "\"" + noRetweet + fromMe;
+    plugin.twitterClient.search(searchQueryFromMe, 200, (err,tweets) => {
+        let mentioned = [];
+        if (!err) {// get already mentioned users
+            tweets.forEach((t) => {
+                if (!t.entities || !t.entities.user_mentions) {
+                  return;
+                }
+                t.entities.user_mentions.forEach((mention) => {
+                    mentioned.push(mention.screen_name);
+                });
+            });
+            plugin.logDebug("Exclude already mentioned users => " + mentioned.join(', '));
+        }
+        let searchQueryNotMe = "\"" + behavior.search + "\"" + noRetweet + notMe;
+        mentioned.forEach((mentionedUser) => {// exclude alreadyMentioned
+            searchQueryNotMe += " -from:" + mentionedUser;
+        });
+        plugin.twitterClient.search(searchQueryNotMe, 20, cb);
+    });
   }
 
   replyTweet(tweet, behavior, cb) {
